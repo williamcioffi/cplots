0daf10fa757b51ca14e7781c4889284d71de6611
williamcioffi <william.cioffi@gmail.com>
2018-11-18 21:41:28 -0500

    las = 1

--- normal_l_axis.R
+++ normal_l_axis.R
@@ -8,8 +8,8 @@ p.usr <- par()$usr
 p.yaxp <- par()$yaxp
 p.xaxp <- par()$xaxp
 
-axis(1, xaxp = c(p.usr[1], p.xaxp[2], p.xaxp[3]), labels = FALSE, tcl = 0)
+axis(1, xaxp = c(p.usr[1], p.xaxp[2], p.xaxp[3]), labels = FALSE, tcl = 0, las = 1)
 axis(1)
 
-axis(2, yaxp = c(p.usr[3], p.yaxp[2], p.yaxp[3]), labels = FALSE, tcl = 0)
+axis(2, yaxp = c(p.usr[3], p.yaxp[2], p.yaxp[3]), labels = FALSE, tcl = 0, las = 1)
 axis(2)
